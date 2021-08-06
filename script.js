f2201e1ba63046bef0960f826df0686b037ee1e5
happyhar <65075165+happyhar@users.noreply.github.com>
2021-08-06 23:14:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let weather = {
+    apikey:"fc083cb5547af6306022a44c21003c96",
+    fetchWeather:function(city){
+        fetch("http://api.openweathermap.org/data/2.5/weather?q="
+        +city
+        +"&units=metric&appid="
+        +this.apikey
+        )
+        .then((response)=> response.json())
+        .then((data)=>this.displayWeather(data));
+    },
+    displayWeather:function(data){
+        const {name} = data;
+        const {icon,description} = data.weather[0];
+        const {temp,humidity} = data.main;
+        const {speed} = data.wind;
+        // console.log(name,icon,description,temp,humidity);
+        document.querySelector(".city").innerText = " Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
+        document.querySelector(".temp").innerText = temp  + " °C";
+        document.querySelector(".desc").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity : "+humidity;
+        document.querySelector(".wind").innerText = "Wind Speed : "+speed;
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1980x1020/?"+ name +"')"
+    },
+    search:function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+document.querySelector(".search button").addEventListener("click",function(){
+weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+    });
+
+weather.fetchWeather("Bangalore");
