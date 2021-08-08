cbd0c073cb51d60f2fd714da1c07e202771d2480
happyhar <65075165+happyhar@users.noreply.github.com>
2021-08-08 13:01:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weather = {
     apikey:"fc083cb5547af6306022a44c21003c96",
     fetchWeather:function(city){
-        fetch("http://api.openweathermap.org/data/2.5/weather?q="
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
         +city
         +"&units=metric&appid="
         +this.apikey
